b6e92e7a430756d6c3ddca29018975007d7ee66d
lubiomer <omer.luvian@gmail.com>
2024-04-30 11:39:09 +0300

    start working

---
+++ TaskAdapter.kt
@@ -0,0 +1,31 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class TaskAdapter(private val context: Context, private val taskList: List<String>) : BaseAdapter() {
+
+    override fun getCount(): Int {
+        return taskList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return taskList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view = convertView
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        }
+        val taskTextView = view?.findViewById<TextView>(android.R.id.text1)
+        taskTextView?.text = taskList[position]
+        return view!!
+    }
+}
